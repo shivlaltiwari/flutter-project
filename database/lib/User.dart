6647d92a4b293b6a8f8bb77ac208cdadf6691227
tiwari <st172239@gmail.com>
2021-04-19 08:25:16 +0545

    user_classomplete

--- database/lib/User.dart
+++ database/lib/User.dart
@@ -1,4 +1,31 @@
-
+import 'dart:core';
 class User{
-  
+  String _colname;
+  String _colpword;
+  int _colid;
+
+  User(this._colname, this._colpword, this._colid);
+  User.map(dynamic obj){
+    this._colname = obj['username'];
+    this._colpword = obj['password'];
+    this._colid = obj['id'];
+
+  }
+  String get username  => _colname;
+  String get password => _colpword;
+  int get id => _colid;
+  Map<String, dynamic> toMap(){
+    var map = new Map <String, dynamic>();
+    map['username']= _colname;
+    map['password'] = _colpword;
+    if (id != null){
+      map["id"] = _colid;
+     }
+    return map;
+  }
+  User.fromMap(Map<String,dynamic>map){
+    this._colname = map['username'];
+    this._colpword = map['passsword'];
+    this._colid = map['id'];
+  }
 }
